08c7ea4294e08cd3d9122a3f673dd9ce49808097
Somme Sakounthong <s.sakounthong@kainos.com>
2014-02-08 11:06:52 +0000

    css

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,12 @@
 $(function() {
   // placeholder for old browsers
   $('input, textarea').placeholder();
-});
 
-
-// toggle menu icon
+  // toggle menu icon
 $(document).on('click.bs.collapse.data-api', '[data-toggle=collapse]', function (e) {
 	$('.menu-btn span').toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
 })
+
+});
+
+
